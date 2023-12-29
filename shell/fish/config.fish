483e2ef3d6cae72b1c6c705bb0c5dd8007c1d200
Nathan PERIER <periernathan2000@gmail.com>
2023-12-29 20:58:43 +0100

    Added base for the fish configuration

---
+++ shell/fish/config.fish
@@ -0,0 +1,20 @@
+#!/usr/bin/fish
+
+if not contains ~/.local/bin $PATH
+	set PATH $PATH ~/.local/bin
+end
+
+
+
+if test -f ~/.config/fish/variables.fish
+	. ~/.config/fish/variables.fish
+end
+
+if test -f ~/.config/fish/local.fish
+	. ~/.config/fish/local.fish
+end
+
+if test -f ~/.config/fish/aliases.fish
+	. ~/.config/fish/aliases.fish
+end
+
