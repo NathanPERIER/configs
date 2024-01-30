7574c37cbe24a6c9c3bbd6fd7e20b835fe84eaa6
NathanPERIER <periernathan2000@gmail.com>
2024-01-30 12:33:22 +0100

    Added an option for a shorten path in bash prompt

--- shell/bash/bash_prompt.sh
+++ shell/bash/bash_prompt.sh
@@ -19,6 +19,7 @@ if [[ -z "$path_colour_code" ]]; then
 	path_colour_code='34' # blue
 fi
 
+
 if [[ -z "$prompt_style" ]]; then
 	prompt_style='default'
 fi
@@ -28,27 +29,39 @@ if [[ -z "$colour_prompt_style" ]]; then
 fi
 
 
+prompt_user='\u'
+if [[ -n "$SUDO_USER" ]]; then
+	prompt_user="($SUDO_USER as \u)"
+fi
+
+prompt_path='\w'
+if [[ -n "$prompt_shorten_path" ]]; then
+	if [[ "$prompt_shorten_path" = 'true' ]] && which pwd-short > /dev/null; then
+		prompt_path='$(pwd-short)'
+	fi
+	unset prompt_shorten_path
+fi
+
 
 if [[ "$colour_support" = yes ]]; then
-	prompt_user='\u'
-	if [[ -n "$SUDO_USER" ]]; then
-		prompt_user="($SUDO_USER as \u)"
-	fi
 	if [[ "$colour_prompt_style" = 'local' ]]; then
 		: # previously set in `.bash_local` and must not be changed
 	elif [[ "$colour_prompt_style" = 'multiline' ]]; then
-		PS1='$(e="$?"; [ "$e" != 0 ] && err="\[\033[0;31m\](\[\033[01;31m\]${e}\[\033[0;31m\])\[\033[0m\] "; auto_newline; date +"╭── [%H:%M:%S] ${err}\[\033['"01;${path_colour_code}m"'\]\w\[\033[00m\]\n╰─ ")\[\033['"01;${user_colour_code}m\\]${prompt_user}"'\[\033['"0;${user_colour_code}m"'\]@\[\033['"01;${user_colour_code}m"'\]\h\[\033[00m\]\$ '
+		PS1='$(e="$?"; [ "$e" != 0 ] && err="\[\033[0;31m\](\[\033[01;31m\]${e}\[\033[0;31m\])\[\033[0m\] "; auto_newline; date +"╭── [%H:%M:%S] ${err}\[\033['"01;${path_colour_code}m\]${prompt_path}"'\[\033[00m\]\n╰─ ")\[\033['"01;${user_colour_code}m\\]${prompt_user}"'\[\033['"0;${user_colour_code}m"'\]@\[\033['"01;${user_colour_code}m"'\]\h\[\033[00m\]\$ '
 	elif [[ "$colour_prompt_style" = 'owo' ]]; then
-		PS1='$(if [ $? = 0 ]; then auto_newline; echo '"'[\\[\033[01;32m\]OwO\[\033[0m\\]'; else auto_newline; echo '[\[\033[01;31m\]╹∧╹\[\e[0m\]'; fi)] \[\033[01;${user_colour_code}m\]${prompt_user}@\h\[\033[00m\]:\[\033[01;${path_colour_code}m\]\w\[\033[00m\]\$ "
+		PS1='$(if [ $? = 0 ]; then auto_newline; echo '"'[\\[\033[01;32m\]OwO\[\033[0m\\]'; else auto_newline; echo '[\[\033[01;31m\]╹∧╹\[\e[0m\]'; fi)] \[\033[01;${user_colour_code}m\]${prompt_user}@\h\[\033[00m\]:\[\033[01;${path_colour_code}m\]${prompt_path}\[\033[00m\]\$ "
 	elif [[ "$colour_prompt_style" = 'basic' ]]; then
-		PS1="${debian_chroot:+($debian_chroot)}"'\[\033['"01;${user_colour_code}m"'\]\u@\h\[\033[00m\]:\[\033['"01;${path_colour_code}m"'\]\w\[\033[00m\]\$ '
+		PS1="${debian_chroot:+($debian_chroot)}"'\[\033['"01;${user_colour_code}m"'\]\u@\h\[\033[00m\]:\[\033['"01;${path_colour_code}m\]\w"'\[\033[00m\]\$ '
 	else # default
-		PS1="${debian_chroot:+($debian_chroot)}"'\[\033['"01;${user_colour_code}m"'\]'"${prompt_user}"'\[\033['"00;${user_colour_code}m"'\]@\[\033['"01;${user_colour_code}m"'\]\h\[\033[00m\]:\[\033['"01;${path_colour_code}m"'\]\w\[\033[00m\]\$ '
+		PS1="${debian_chroot:+($debian_chroot)}"'\[\033['"01;${user_colour_code}m"'\]'"${prompt_user}"'\[\033['"00;${user_colour_code}m"'\]@\[\033['"01;${user_colour_code}m"'\]\h\[\033[00m\]:\[\033['"01;${path_colour_code}m\]${prompt_path}"'\[\033[00m\]\$ '
 	fi
-	unset prompt_user
 else
-	if [[ "$prompt_style" != 'local' ]]; then
+	if [[ "$prompt_style" = 'local' ]]; then
+		:
+	elif [[ "$prompt_style" = 'basic' ]]; then
 		PS1="${debian_chroot:+($debian_chroot)}"'\u@\h:\w\$ '
+	else # default
+		PS1="${debian_chroot:+($debian_chroot)}${prompt_user}"'\u@'"$prompt_path"':\w\$ '
 	fi
 fi
 
@@ -57,6 +70,8 @@ unset colour_prompt_style
 unset prompt_style
 unset user_colour_code
 unset path_colour_code
+unset prompt_user
+unset prompt_path
 
 
 
