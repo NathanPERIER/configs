7b14aa059cadddc5f7fc431503a77e90a1504568
NathanPERIER <periernathan2000@gmail.com>
2024-02-09 20:33:57 +0100

    Added check for detached HEAD state on force pull and force push

--- command-line/git/scripts/git-fpull.sh
+++ command-line/git/scripts/git-fpull.sh
@@ -2,15 +2,20 @@
 # usage : ./git-fpull.sh
 
 current_branch=$(git branch --show-current)
-
+if [[ -z "$current_branch" ]]; then
+    current_commit=$(git rev-parse --short HEAD)
+    echo "Currently in detached HEAD state on commit $current_commit"
+    exit 1
+fi
 
 remote_branch=$(git status -sb | head -n 1 | sed -r 's/##\s.*\.\.\.(.*)/\1/')
 if [[ "$remote_branch" = '##'* ]]; then
     echo "Currently on branch $current_branch with no upstream"
-    return 1
+    exit 1
 fi
 
 echo "You are currently on branch $current_branch tracking $remote_branch"
+
 
 read -p "Do you really want to erase all local changes and reset the history to match the remote history ? (n/Y) " yn
 if [[ "$yn" = 'Y' ]]; then 
--- command-line/git/scripts/git-fpush.sh
+++ command-line/git/scripts/git-fpush.sh
@@ -2,12 +2,17 @@
 # usage : ./git-fpush.sh
 
 current_branch=$(git branch --show-current)
+if [[ -z "$current_branch" ]]; then
+    current_commit=$(git rev-parse --short HEAD)
+    echo "Currently in detached HEAD state on commit $current_commit"
+    exit 1
+fi
 echo "You are currently on branch $current_branch"
 
 
 read -p "Do you really want to force an update on the origin ? (n/Y) " yn
-if [[ "yn" = 'Y' ]]; then 
-    git push -f \"$@\"
+if [[ "$yn" = 'Y' ]]; then 
+    git push -f "$@"
     exit $?
 else
     echo 'Canceled'
