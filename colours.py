ae51f2ab03e89c1918dba23bd972e4b0b1b4d79c
NathanPERIER <periernathan2000@gmail.com>
2024-11-06 01:59:39 +0100

    WIP: added script to generate the colour variables

--- colours.py
+++ colours.py
@@ -2,11 +2,20 @@
 
 import re
 import sys
+import traceback
+
+import yaml
+
+
+def help():
+    # TODO [bash|fish]
+    print(f"usage: {sys.argv[0]} <classes> <colours>")
 
 
 col_reg = re.compile(r'(clear|bold|dim|italic|underline|strikethrough)|([fb]g:)?(?:((?:bright-)?(?:black|red|green|yellow|blue|magenta|cyan|white|default)|dark-grey)|#([0-9a-fA-F]{6})|([0-1]?[0-9]{1,2}|2[0-4][0-9]|25[0-5]))')
+raw_code_reg = re.compile(r'[0-9]+(?:;[0-9]+)*')
 
-def convert_colour(name: str) -> str :
+def convert_colour_component(name: str) -> str :
     m = col_reg.fullmatch(name)
     if m is None :
         return ''
@@ -69,16 +78,114 @@ def convert_colour(name: str) -> str :
     return f"{30 + background_val + bright_val + col_val}"
 
 
-def main():
-    res = []
-    for repr in sys.argv[1].split() :
-        col_code = convert_colour(repr.strip())
+def convert_colour(repr: str) -> str :
+    if raw_code_reg.fullmatch(repr) is not None :
+        return repr
+    res: list[str] = []
+    for cpt in repr.split() :
+        cpt_code = convert_colour_component(cpt)
+        if len(cpt_code) > 0 :
+            res.append(cpt_code)
+    return ";".join(res)
+
+
+def load_yaml(path: str):
+    with open(path, 'r') as f:
+        return yaml.safe_load(f)
+
+
+ls_types_names = {
+    'normal':                'no',
+    'file':                  'fi',
+    'reset':                 'rs',
+    'directory':             'di',
+    'multi_hard_link':       'mh',
+    'sym_link':              'ln',
+    'orphan':                'or',
+    'missing':               'mi',
+    'fifo':                  'pi',
+    'socket':                'so',
+    'door':                  'do',
+    'block_device':          'bd',
+    'char_device':           'cd',
+    'setuid':                'su',
+    'setgid':                'sg',
+    'capability':            'ca',
+    'sticky_other_writable': 'tw',
+    'other_writable':        'ow',
+    'sticky':                'wt',
+    'executable':            'ex'
+}
+
+
+def print_gcc_var(gcc_colours: dict[str, str]):
+    res: list[str] = []
+    for elt, col_repr in gcc_colours.items() :
+        col_code = convert_colour(col_repr)
         if len(col_code) > 0 :
-            res.append(col_code)
+            res.append(f"{elt}={col_code}")
     if len(res) > 0 :
-        full_code = ";".join(res)
-        print(full_code)
-        print(f"\x1b[{full_code}mtest\x1b[0m")
+        print(f"export GCC_COLORS='{':'.join(res)}'")
+        print('gcc_colors_ok=true')
+
+def print_ls_var(classes, colours) :
+    ls_classes = {}
+    for cls_id, cls in classes.items() :
+        patterns = []
+        default_code = convert_colour(cls['default'])
+        if len(default_code) == 0 :
+            default_code = '0'
+        if 'extensions' in cls :
+            patterns.extend(f"*.{ext}" for ext in cls['extensions'])
+        if 'patterns' in cls :
+            patterns.extend(cls['patterns'])
+        if len(patterns) > 0 :
+            ls_classes[cls_id] = (default_code, patterns)
+    if 'classes' in colours :
+        for cls_id, col_repr in colours['classes'].items() :
+            col_code = convert_colour(col_repr)
+            if len(col_code) > 0 and cls_id in ls_classes :
+                ls_classes[cls_id] = (col_code, ls_classes[cls_id][1])
+    res = []
+    if 'types' in colours :
+        for type_name, col_repr in colours['types'].items() :
+            col_code = convert_colour(col_repr)
+            if type_name in ls_types_names and len(col_code) > 0 :
+                res.append(f"{ls_types_names[type_name]}={col_code}")
+    for col_code, patterns in ls_classes.values() :
+        for p in patterns :
+            res.append(f"{p}={col_code}")
+    print(f"export LS_COLORS='{':'.join(res)}'")
+    print('ls_colors_ok=true')
+    
+
+def print_vars(classes, colours) :
+    if 'gcc_colours' in colours :
+        print_gcc_var(colours['gcc_colours'])
+    if 'ls_colours' in colours :
+        print_ls_var(classes, colours['ls_colours'])
+
+
+
+
+def main():
+    if len(sys.argv) > 1 and sys.argv[1] in ['-h', '--help'] :
+        help()
+        sys.exit(0)
+    
+    if len(sys.argv) != 3 :
+        sys.exit(1)
+
+    try:
+        classes = load_yaml(sys.argv[1])
+        colours = load_yaml(sys.argv[2])
+        print_vars(classes, colours)
+        # code = convert_colour(sys.argv[1])
+        # if len(code) > 0 :
+        #     print(code)
+        #     print(f"\x1b[{code}mtest\x1b[0m")
+    except Exception:
+        print(traceback.format_exc(), file=sys.stderr)
         
 
 
